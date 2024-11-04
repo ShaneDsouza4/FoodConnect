c1a8bf386d2ad4eddab790d1c53a2b55b6a83935
ShaneDsouza4 <shane.json96@gmail.com>
2024-11-03 22:12:50 -0500

    Added feild isVerified for id card

--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     id_verification = models.ImageField(upload_to='id_cards/', blank=True, null=True)  # ID card image
+    is_verified = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.user.username}'s profile"
