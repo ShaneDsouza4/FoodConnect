0712cf70e6462737f90b0bf5829ef82ee746c0dc
jsmngill <gilljasmine595@gmail.com>
2024-11-04 12:11:46 -0500

    Base Model for alerts added

--- alerts/models.py
+++ alerts/models.py
@@ -1,3 +1,13 @@
-from django.db import models
-
 # Create your models here.
+# alerts/models.py
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class EmergencyAlert(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="emergency_alerts")
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True) #For one time saving purppposes
+
+    def _str_(self):
+        return f"Emergency Alert from {self.user.username} created on {self.created_at}"
