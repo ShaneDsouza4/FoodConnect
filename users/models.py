796ab1c0f621453de684b5a2d0f8cdcaadbef7af
ShaneDsouza4 <shane.json96@gmail.com>
2024-11-06 22:56:10 -0500

    User Models updated for Donations and Analytics task, Forgot Password implemented with gmail keys

--- users/models.py
+++ users/models.py
@@ -9,12 +9,9 @@ ROLE_CHOICES = [
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    #id_verification = models.ImageField(upload_to='id_cards/', blank=True, null=True)  # ID card image
-    #is_verified = models.BooleanField(default=False)
-
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='individual')
 
-    # General Feilds for the form
+    # General
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     street_address = models.CharField(max_length=255, blank=True, null=True)
     city = models.CharField(max_length=100, blank=True, null=True)
@@ -23,11 +20,19 @@ class Profile(models.Model):
     id_verification = models.ImageField(upload_to='id_cards/', blank=True, null=True)
     is_verified = models.BooleanField(default=False)
 
-    # Role specific fields
+    # Restaurant
     restaurant_name = models.CharField(max_length=255, blank=True, null=True)
     restaurant_contact_number = models.CharField(max_length=50, blank=True, null=True)
+    total_donations = models.IntegerField(default=0)
+    average_rating = models.FloatField(blank=True, null=True)
+
+    # Foodbank
     foodbank_name = models.CharField(max_length=255, blank=True, null=True)
     foodbank_contact_number = models.CharField(max_length=50, blank=True, null=True)
+    total_reservations = models.IntegerField(default=0)
+    emergency_alerts_count = models.IntegerField(default=0)
+
+    reviews_given = models.IntegerField(blank=True, null=True)
 
 def __str__(self):
         return f"{self.user.username}'s profile"
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import signup_view, home_view, landing_view, login_view, logout_view, emergency_alert_view, donate_view
-
+from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', landing_view, name='landing'),
     path('home/', home_view, name='home'),
@@ -9,6 +9,10 @@ urlpatterns = [
     path('login/', login_view, name='login'),
 
     path('logout/', logout_view, name='logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset_form.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
     path('emergency_alert/', emergency_alert_view, name='emergency_alert'),
     path('donate/', donate_view, name='donate'),
 ]
